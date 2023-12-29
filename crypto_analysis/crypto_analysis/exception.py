b08b62688d0879b4b6b0695bd803da2cb6b6f42b
Aziz el Kadi <azizelkadi24@gmail.com>
2023-12-29 17:17:08 +0100

    added exception test

--- crypto_analysis/crypto_analysis/exception.py
+++ crypto_analysis/crypto_analysis/exception.py
@@ -6,17 +6,16 @@ class CryptoAnalysisException(Exception):
     def __init__(self, error_message, error_location):
         super().__init__(error_message)
         # Build error message
-        self.error_message = self.error_message_detail(error_message, error_location)
+        self.error_message_detail(error_message, error_location)
 
     def __str__(self):
         # Show error message
         return self.error_message
 
-    @staticmethod
-    def error_message_detail(error, error_location):
+    def error_message_detail(self, error, error_location):
         """
         Creates a detailed error message with the given error and error location.
         """
         # Concatenate error location to error message
         error_message = f"Error ocurred [{error_location}] -> {str(error)}"
-        return error_message
+        self.error_message = error_message
